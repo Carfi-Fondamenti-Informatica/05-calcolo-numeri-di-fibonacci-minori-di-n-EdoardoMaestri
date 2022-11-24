b8d046d708625c7775bc0ff461a899e800d2e255
EdoardoMaestri <115471996+EdoardoMaestri@users.noreply.github.com>
2022-11-24 21:04:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+
+        int a = 1;
+        int b = 1;
+        int c;
+
+        while (a<=n)
+        {
+            cout << a << endl;
+            c = a;
+            a = b;
+            b = a + c;
+        }
    return 0;
 }
